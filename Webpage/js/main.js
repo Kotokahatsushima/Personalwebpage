f5ef6f33ddec1ee66ea2e75db6902c5a9ac44ac6
Kotokahatsushima <Kotokahatsushima@users.noreply.github.com>
2023-11-20 23:29:49 +0800

    Update

---
+++ Webpage/js/main.js
@@ -0,0 +1,26 @@
+var commentButton = document.getElementById("commentButton");
+var inputComment = document.getElementById("inputComment");
+var mainCanvas = document.getElementsByClassName("allcomments")[0]
+commentButton.addEventListener("click", addComment);
+function addComment(){
+    var commentwrap = document.createElement("div");
+    commentwrap.className = "commentwrap";
+    commentwrap.id = "commentwrap" + Date.now();
+    mainCanvas.appendChild(commentwrap);
+    var comment = inputComment.value;
+    var commentAsString = `
+                <div class="profile">
+                    <img src="https://i.pinimg.com/originals/c0/27/be/c027bec07c2dc08b9df60921dfd539bd.webp" alt="">
+                    <h3>You</h3>
+                </div>
+                <p class="time">Just Now</p>
+                <div class="comment">
+                    <p>${comment}</p>
+                </div>
+                <hr>
+    `;
+    document.getElementById(commentwrap.id).innerHTML = commentAsString;
+    inputComment.value = "";
+    
+
+}
